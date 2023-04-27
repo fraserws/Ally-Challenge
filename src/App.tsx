39eb33e1f772173e5014927d485c745c30862ce0
fraserws <fraserws.uk2@gmail.com>
2023-04-27 15:37:57 +0100

    skeleton + base components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import React, { useState } from "react";
+import ButtonGroup from "./components/buttongroup";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
-    <div className="w-screen h-full flex flex-col justify-center items-center">
-      <h1 className="text-4xl">Hello there</h1>
+    <div className="w-screen">
+      <Navbar />
+      <main className="mx-auto flex min-h-screen max-w-[1500px]">
+        <div className="fixed  h-full flex-col items-center p-2 sm:flex xl:w-[340px] xl:items-start">
+          <ButtonGroup />
+        </div>
+        <div className="max-w-2xl flex-grow overflow-hidden sm:ml-[73px] xl:ml-[370px] ">
+          section2
+        </div>
+        <div className="ml-8 hidden space-y-5 py-1 lg:inline xl:w-[450px]"></div>
+      </main>
     </div>
   );
 }
---
+++ src/components/buttongroup.tsx
@@ -0,0 +1,28 @@
+import { BellIcon, BookmarkIcon, HomeIcon, UserIcon } from "lucide-react";
+import { Button } from "./ui/button";
+
+function ButtonGroup() {
+  return (
+    <div className="flex flex-row sm:flex-col items-start">
+      <Button variant="ghost" className="flex items-center">
+        <HomeIcon className="sm:mr-2" />
+        <span className="hidden sm:block">Home</span>
+      </Button>
+      <Button variant="ghost" className="flex items-center">
+        <BellIcon className="sm:mr-2" />
+        <span className="hidden sm:block">Notifications</span>
+      </Button>
+      <Button variant="ghost" className="flex items-center">
+        <BookmarkIcon className="sm:mr-2" />
+        <span className="hidden sm:block">Saves</span>
+      </Button>
+
+      <Button variant="ghost" className="flex items-center">
+        <UserIcon className="sm:mr-2" />
+        <span className="hidden sm:block">Profile</span>
+      </Button>
+    </div>
+  );
+}
+
+export default ButtonGroup;
